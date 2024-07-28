a7d602e2b1ed964982d7e6db136ed18ea00e0c5c
glenngt <glenn.mattackal@gmail.com>
2024-07-28 18:29:29 +0530

    Partially completed the routing file addition

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Homepage from './Pages/homepage/homepage'; // Ensure this matches the actual file name and path
+import DashboardPage from './Pages/dashboardPage/dashboardPage'; // Ensure this matches the actual file name and path
+import ChatPage from './Pages/chatPage/hatPage'; // Ensure this matches the actual file name and path
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Homepage />,
+  },
+  {
+    path: '/dashboard',
+    children: [
+      { path: '/dashboard', element: <DashboardPage /> },
+      { path: '/dashboard/chats/:id', element: <ChatPage /> }
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
