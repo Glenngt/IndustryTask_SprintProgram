371f079009e4a2d200bca70219b6b1ac87287421
glenngt <glenn.mattackal@gmail.com>
2024-07-29 17:19:20 +0530

    Home Page completed

--- src/Pages/homepage/Homepage.jsx
+++ src/Pages/homepage/Homepage.jsx
@@ -4,7 +4,29 @@ import './homepage.css'
 const Homepage = () => {
   return (
     <div className='homepage'>
-      <Link to = "/dashboard">Dashboard</Link>
+      <img src="/orbital.png" alt="" className='orbital' />
+      <div className="left">
+        <h1>CHAT-VEME</h1>
+        <h2>Unleash Conversations, Elevate Ideas.</h2>
+        <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci aspernatur iste enim quaerat reiciendis distinctio laudantium omnis officiis libero ad, obcaecati voluptatem nobis iure, minus vitae voluptate accusamus atque.</h3>
+        <Link to="/dashboard">Get Started</Link>
+      </div>
+      <div className="right">
+        <div className="imgContainer">
+          <div className="bgContainer">
+            <div className="bg"></div>
+          </div>
+          <img src="/bot.png" alt="" className='bot' />
+        </div>
+      </div>
+      <div className="terms">
+      <img src="/logo.png" alt="" />
+      <div className="links">
+        <Link to = "/">Terms of Service</Link>
+        <span>|</span>
+        <Link  to = "/">Privacy Policy</Link>
+      </div>
+      </div>
     </div>
   )
 }
