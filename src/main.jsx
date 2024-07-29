f8ff4c45b3ca07ad867f2134f522fd13e36f8dbc
glenngt <glenn.mattackal@gmail.com>
2024-07-29 13:04:19 +0530

    Page routing completed

--- src/main.jsx
+++ src/main.jsx
@@ -5,18 +5,31 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Homepage from './Pages/homepage/homepage'; // Ensure this matches the actual file name and path
 import DashboardPage from './Pages/dashboardPage/dashboardPage'; // Ensure this matches the actual file name and path
 import ChatPage from './Pages/chatPage/chatPage';
+import RootLayout from './layouts/rootLayout/rootLayout';
+import DashboardLayout from './layouts/dashboardLayout/dashboardLayout';
 
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Homepage />,
-  },
-  {
-    path: '/dashboard',
+    element: <RootLayout />,
     children: [
-      { path: '/dashboard', element: <DashboardPage /> },
-      { path: '/dashboard/chats/:id', element: <ChatPage /> }
+      {
+        path: "/",
+        element: <Homepage />,
+      },
+      {
+        element: <DashboardLayout/>,
+        children: [
+          {
+            path: "/dashboard",
+            element: <DashboardPage/>,
+          },
+          {
+            path: "/dashboard/chats/:id",
+            element: <ChatPage/>,
+          }
+        ]
+      }
     ]
   },
 ]);
