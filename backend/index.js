f85a600d2d0eed7939be2ebf1a52d4f438797529
glenngt <glenn.mattackal@gmail.com>
2024-07-30 18:00:04 +0530

    Data Retriving Done

--- backend/index.js
+++ backend/index.js
@@ -92,11 +92,54 @@ app.post("/api/chats", ClerkExpressRequireAuth(), async (req, res) => {
 app.get("/api/userchats", ClerkExpressRequireAuth(), async (req, res) => {
     const userId = req.auth.userId;
     try {
-        const userChats = await UserChats.find({userId});
+        const userChats = await UserChats.find({ userId });
         res.status(200).send(userChats[0].chats);
     } catch (err) {
         console.log(err);
         res.status(500).send("Error fetching Userchats!!");
+    }
+})
+
+app.get("/api/chats/:id", ClerkExpressRequireAuth(), async (req, res) => {
+    const userId = req.auth.userId;
+    try {
+        const chat = await Chat.findOne({ _id: req.params.id, userId });
+        res.status(200).send(chat);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("Error fetching Chat!!");
+    }
+})
+
+app.put("/api/chats/:id", ClerkExpressRequireAuth(), async (req, res) => {
+    const userId = req.auth.userId;
+
+    const { question, answer, img } = req.body;
+    const newItems = [
+        ...(question ? [{
+            role: "user",
+            parts: [{ text: question }],
+            ...(img && { img })
+        }] : []),
+        {
+            role: "model",
+            parts: [{ text: answer }],
+        }
+    ];
+
+
+    try {
+        const updatedChat = await Chat.updateOne({ _id: req.params.id, userId }, {
+            $push: {
+                history: {
+                    $each: newItems
+                }
+            }
+        });
+        res.status(200).send(updatedChat);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("Error adding Conversations!!");
     }
 })
 
